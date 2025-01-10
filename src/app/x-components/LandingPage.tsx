f9940aa35fdd84fc5763687b8ab0d28c28ff1887
Daniel Gavidia <dgavidia1@gmail.com>
2025-01-10 10:10:04 -0500

    Finished basic styling of landing page

--- src/app/x-components/LandingPage.tsx
+++ src/app/x-components/LandingPage.tsx
@@ -1,15 +1,18 @@
+import { House } from "lucide-react";
+
 const LandingPage = () => {
   const navButtons = ["For Schools", "Internship", "FAQ", "Blog", "Get AI Certified"];
   return (
-    <div className="w-full p-4">
+    <div className="w-full p-4 flex flex-col space-y-4">
       {/* Navbar */}
-      <div className="flex items-center justify-between border-[0.5px] rounded-3xl p-2">
+      <div className="flex items-center justify-between border-[0.5px] rounded-3xl p-2 px-8">
         {/* Nav buttons */}
-        <div className="flex space-x-3 flex-1">
+        <div className="flex space-x-3 flex-1 font-semibold">
+          <House />
           {navButtons.map((b, i) => (
-            <p key={i} className="text-sm">
+            <div key={i} className="text-sm flex items-center justify-center">
               {b}
-            </p>
+            </div>
           ))}
         </div>
         {/* Logo */}
@@ -22,10 +25,38 @@ const LandingPage = () => {
         </div>
         {/* Join button */}
         <div className="flex-1 flex justify-end">
-          <button className="rounded-3xl">Join</button>
+          <button className="rounded-3xl border-[0.5px] p-3 w-28 text-sm bg-yellow-100 font-semibold">
+            Join
+          </button>
         </div>
       </div>
+
       {/* Card */}
+      <div className="flex border-[0.5px] rounded-3xl p-2">
+        {/* Left Section */}
+        <div className="w-1/2 p-14 h-full flex flex-col justify-between">
+          <p className="text-6xl font-bold">AI-Powered Storytelling for Your Future</p>
+          <p className="font-semibold">
+            Applying to college? Join over 300,000 students saving time and money building their
+            story with ESAI. Brainstorm topics, outline essays, edit writing, match skills to
+            specific schools and more. ESAI's library of tools help you create a custom strategy so
+            you can stand out, and get in!
+          </p>
+          <button className="rounded-3xl border-[0.5px] p-3 w-44 text-sm bg-yellow-100 font-semibold">
+            Join
+          </button>
+        </div>
+        {/* Right Section */}
+        <div className="w-1/2 p-4">
+          <div className="h-[500px] overflow-hidden rounded-3xl">
+            <img
+              src="https://strapi-aws-images.s3.us-east-1.amazonaws.com/intro_banner_281dfaa097.jpeg?w=1200&q=75"
+              alt="User"
+              className="w-full h-full object-cover object-[center_40%]"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
